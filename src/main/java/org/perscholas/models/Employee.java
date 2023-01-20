fb0cb934112ebab31039d96fe34782944cd00983
Suganya <suganya.venkat88@gmail.com>
2023-01-20 15:24:30 -0500

    orm project

---
+++ src/main/java/org/perscholas/models/Employee.java
@@ -0,0 +1,47 @@
+package org.perscholas.models;
+
+import java.util.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "emp")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	@NonNull
+	@Column(columnDefinition = "varchar(50)")
+	String name;
+	@NonNull
+	Date dob;
+	@NonNull
+	String password;
+	/** public Employee(@NonNull String name, @NonNull Date dob, @NonNull String password) {
+		super();
+		this.name = name;
+		this.dob = dob;
+		this.password = password;
+	}**/
+
+}
